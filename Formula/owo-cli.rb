f8f5aad55aa9505ba3245f12bf37fcb043091b80
Alec Branaa <ibealec@gmail.com>
2026-02-05 16:29:36 -0800

    Update Homebrew formula for v1.1.3

--- Formula/owo-cli.rb
+++ Formula/owo-cli.rb
@@ -1,22 +1,22 @@
 class OwoCli < Formula
   desc "Natural language to shell commands using AI"
   homepage "https://github.com/ibealec/owo"
-  version "1.1.1"
+  version "1.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ibealec/owo/releases/download/v#{version}/owo-cli-darwin-arm64"
-      sha256 "cac0285e58a4380207ab06fe17dfa5bcfd2935ea5480d6ac4ce337230259ff0d"
+      sha256 "85e52ca1d2775f8f210b92d7e2d705b850d3b39ffdbb2d29bb3b645868b54434"
     else
       url "https://github.com/ibealec/owo/releases/download/v#{version}/owo-cli-darwin-x64"
-      sha256 "4a8a7d735596187b8e8a486cf848c6b7f7dc79c1af77a850d7c2c0cfc6e9d7fe"
+      sha256 "5eca0ac7cac65a952fa0f360c6a5960feb9abb0de636fdf90c28d5b5b4d8abe3"
     end
   end
 
   on_linux do
     url "https://github.com/ibealec/owo/releases/download/v#{version}/owo-cli-linux-x64"
-    sha256 "acf3bfe498bb28e004edbeb54afa7cdcf2a0a24ba8f1b4af65a22c215ee3f644"
+    sha256 "459ebaf0a5e3942b1ce637dd82408d9cca11d313d0e3112fb98e1a688bfbc875"
   end
 
   def install
