5c9144180956de29b971a33b4da52c45ca6ec5ff
Alec Branaa <ibealec@gmail.com>
2026-02-05 16:19:11 -0800

    Update Homebrew formula for v1.1.1
    Fill in SHA256 hashes from v1.1.1 release binaries.
    Point URLs to ibealec/owo repo.

--- Formula/owo-cli.rb
+++ Formula/owo-cli.rb
@@ -7,16 +7,16 @@ class OwoCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ibealec/owo/releases/download/v#{version}/owo-cli-darwin-arm64"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "cac0285e58a4380207ab06fe17dfa5bcfd2935ea5480d6ac4ce337230259ff0d"
     else
       url "https://github.com/ibealec/owo/releases/download/v#{version}/owo-cli-darwin-x64"
-      sha256 "PLACEHOLDER_DARWIN_X64"
+      sha256 "4a8a7d735596187b8e8a486cf848c6b7f7dc79c1af77a850d7c2c0cfc6e9d7fe"
     end
   end
 
   on_linux do
     url "https://github.com/ibealec/owo/releases/download/v#{version}/owo-cli-linux-x64"
-    sha256 "PLACEHOLDER_LINUX_X64"
+    sha256 "acf3bfe498bb28e004edbeb54afa7cdcf2a0a24ba8f1b4af65a22c215ee3f644"
   end
 
   def install
